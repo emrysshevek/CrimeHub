77f8c33febd522534ae07e0b7e32de3d3da5c5af
EC2 Default User <ec2-user@ip-172-31-9-164.us-east-2.compute.internal>
2018-12-07 17:20:29 +0000

    added crime schema

--- crime/models/users_model.js
+++ crime/models/users_model.js
@@ -3,7 +3,16 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
     username: { type: String, unique: true },
     email: String,
-    color: String,
+    crimes: [Object]
     hashed_password: String
 });
+var CrimeSchema = new Schema({
+    weapons: [String],
+    targets: [String],
+    location: String,
+    date: Date,
+    bounty: String,
+    value: Number
+})
 mongoose.model('User', UserSchema);
+mongoose.model('Crime', UserSchema);
