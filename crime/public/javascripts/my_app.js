b46b05d3be9a7889ea06da63e5c29da67b0789d6
EC2 Default User <ec2-user@ip-172-31-9-164.us-east-2.compute.internal>
2018-12-11 19:44:00 +0000

    crimes are now visible

--- crime/public/javascripts/my_app.js
+++ crime/public/javascripts/my_app.js
@@ -1,10 +1,13 @@
 angular.module('myApp', []).
 controller('myController', ['$scope', '$http',
   function($scope, $http) {
+    $scope.crimes = [];
     $http.get('/user/profile')
       .success(function(data, status, headers, config) {
         $scope.user = data;
         $scope.error = "";
+        $scope.crimes = data.crimes;
+        console.log($scope.crimes);
       }).
     error(function(data, status, headers, config) {
       $scope.user = {};
